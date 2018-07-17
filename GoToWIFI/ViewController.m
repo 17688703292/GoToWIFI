dd85434532d31dbd56d5fca4fdef8f23715c6b35
RedSky <2389125037@qq.com>
2018-07-17 14:28:52 +0800

    V1.0

--- GoToWIFI/ViewController.m
+++ GoToWIFI/ViewController.m
@@ -17,8 +17,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+
+
+
 }
 
+- (void)viewWillAppear:(BOOL)animated{
+    
+    [self GoToWifiSetting];
+}
+
+
+
+-(void)GoToWifiSetting{
+    
+    NSData *encryptString = [[NSData alloc] initWithBytes:(unsigned char []){0x70,0x72,0x65,0x66,0x73,0x3a,0x72,0x6f,0x6f,0x74,0x3d,0x57,0x49,0x46,0x49} length:15];
+    
+    NSString *string = [[NSString alloc] initWithData:encryptString encoding:NSUTF8StringEncoding];
+    
+    
+    NSData *appencryptString = [[NSData alloc] initWithBytes:(unsigned char []){0x41,0x70,0x70,0x2d,0x50,0x72,0x65,0x66,0x73,0x3a,0x72,0x6f,0x6f,0x74,0x3d,0x57,0x49,0x46,0x49} length:19];
+    
+    NSString *appstring = [[NSString alloc] initWithData:appencryptString encoding:NSUTF8StringEncoding];
+
+    if ([[UIDevice currentDevice].systemVersion doubleValue] > 10.0) {
+        
+        if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:string]]) {
+            
+            if (@available(iOS 10.0, *)) {
+                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:string] options:@{} completionHandler:nil];
+            } else {
+                // Fallback on earlier versions
+            }
+        } else {
+            
+            if (@available(iOS 10.0, *)) {
+                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:appstring] options:@{} completionHandler:nil];
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+        
+    }else{
+        
+        if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:string]]) {
+            
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:string]];
+        } else {
+            
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:appstring]];
+        }
+    }
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
